dc1fb68ed30da3f253adb0251eebb44230df6e70
gpm22 <gabriel.milhomem@vorasys.com.br>
2022-02-07 18:10:35 -0300

    adding method findByModelAndYear

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/VehicleRepository.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/VehicleRepository.java
@@ -4,4 +4,5 @@ import com.github.gpm22.API_Veiculos.Entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    Vehicle findByModelAndYear(String model, String year);
 }
