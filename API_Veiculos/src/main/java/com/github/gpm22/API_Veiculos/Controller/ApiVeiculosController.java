662ccf6b100715c521e6a88a28b5d1f86a80f613
gpm22 <gabriel.milhomem@vorasys.com.br>
2022-02-07 17:31:29 -0300

    adding logs to Controller

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Controller/ApiVeiculosController.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Controller/ApiVeiculosController.java
@@ -4,6 +4,8 @@ import com.github.gpm22.API_Veiculos.Entities.Owner;
 import com.github.gpm22.API_Veiculos.Entities.Vehicle;
 import com.github.gpm22.API_Veiculos.Services.IOwnerService;
 import com.github.gpm22.API_Veiculos.Services.IVehicleService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +16,13 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Set;
+
 @RestController
 @RequestMapping("/apiveiculos/v1")
 public class ApiVeiculosController {
+
+    Logger logger = LoggerFactory.getLogger(ApiVeiculosController.class);
 
     @Autowired
     private IVehicleService vehicleService;
@@ -27,10 +33,15 @@ public class ApiVeiculosController {
     @PostMapping("/usuario")
     public ResponseEntity createOwner(@RequestBody Owner owner) {
         try {
+            logger.info("Iniciando cadastro do usuario: " + owner);
+            Owner newOwner = ownerService.save(owner);
+            logger.info("Cadastro realizado com sucesso do usuario: " + newOwner);
             return ResponseEntity
                     .status(HttpStatus.CREATED)
-                    .body(ownerService.save(owner));
+                    .body(newOwner);
         } catch (IllegalArgumentException e) {
+            logger.error("Erro durante cadastro do usuário:" + owner);
+            e.printStackTrace();
             return ResponseEntity.status(HttpStatus.BAD_REQUEST)
                     .body(e.getMessage());
         }
@@ -39,13 +50,21 @@ public class ApiVeiculosController {
     @GetMapping("/usuario/{email_ou_cpf}")
     public ResponseEntity getOwner(@PathVariable(value="email_ou_cpf") String emailOuCpf) {
         try {
+            logger.info("Solicitado usuário com " + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf );
+
+            Owner owner = ownerService.getByCpfOrEmail(emailOuCpf);
+
+            logger.info("Retornando usuário " + owner);
+
             return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(ownerService.getByCpfOrEmail(emailOuCpf));
+                    .body(owner);
 
         } catch (IllegalArgumentException e){
+            logger.error("Erro ao retornar usuário com " + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf );
+            e.printStackTrace();
             return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT)
+                    .status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
         }
     }
@@ -53,11 +72,19 @@ public class ApiVeiculosController {
     @PostMapping("/cadastrar-veiculo/{email_ou_cpf}")
     public ResponseEntity createVehicle(@PathVariable(value="email_ou_cpf") String emailOuCpf, @RequestBody Vehicle vehicle){
         try {
+            logger.info("O usuário com "  + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf + " solicita o cadastro do veículo " + vehicle  );
+
+            Vehicle newVehicle = vehicleService.save(emailOuCpf, vehicle);
+
+            logger.info("Cadastro realizado com sucesso do veículo: " + newVehicle);
+
             return ResponseEntity
                     .status(HttpStatus.CREATED)
-                    .body(vehicleService.save(emailOuCpf, vehicle));
+                    .body(newVehicle);
 
         } catch (IllegalArgumentException e){
+            logger.error("Erro ao cadastrar veículo: " + vehicle);
+            e.printStackTrace();
             return ResponseEntity
                     .status(HttpStatus.BAD_REQUEST)
                     .body(e.getMessage());
@@ -67,13 +94,21 @@ public class ApiVeiculosController {
     @GetMapping("/lista-de-veiculos/{email_ou_cpf}")
     public ResponseEntity getVehicles(@PathVariable(value="email_ou_cpf") String emailOuCpf){
         try {
+            logger.info("Solicitando lista de veículos do usuário com "  + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf );
+
+            Set<Vehicle> vehicles = vehicleService.getVehiclesByOwner(emailOuCpf);
+
+            logger.info("Retornado veículos do usuário com " + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf + ": " + vehicles);
+
             return ResponseEntity
                     .status(HttpStatus.OK)
-                    .body(vehicleService.getVehiclesByOwner(emailOuCpf));
+                    .body(vehicles);
 
         } catch (IllegalArgumentException e){
+            logger.error("Erro ao retornar lista de veículos do usuário com " + (emailOuCpf.contains("@")? "email " : "cpf ") + emailOuCpf);
+            e.printStackTrace();
             return ResponseEntity
-                    .status(HttpStatus.NO_CONTENT)
+                    .status(HttpStatus.NOT_FOUND)
                     .body(e.getMessage());
         }
     }
