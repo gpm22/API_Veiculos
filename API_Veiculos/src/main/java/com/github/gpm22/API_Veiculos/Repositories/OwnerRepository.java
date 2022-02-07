6946c8a167e9b832d5b93a29f9dd5cb32355c9a8
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-02-07 00:13:57 -0300

    removing unnecessary @Query annotations

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/OwnerRepository.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/OwnerRepository.java
@@ -6,10 +6,8 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
-    @Query(value = "select o from Owner o where o.cpf = ?1")
     Owner findByCpf(final String cpf);
-
-    @Query(value = "select o from Owner o where o.email = ?1")
+    
     Owner findByEmail(final String email);
 
 }
