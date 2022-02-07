216d4726c0b5ae34275f09fb82cf584f49266da1
Gabriel1000homem <gabrielpacmil@gmail.com>
2022-02-07 00:25:08 -0300

    removing import of @Query

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/OwnerRepository.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/OwnerRepository.java
@@ -2,12 +2,11 @@ package com.github.gpm22.API_Veiculos.Repositories;
 
 import com.github.gpm22.API_Veiculos.Entities.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface OwnerRepository extends JpaRepository<Owner, Long> {
 
     Owner findByCpf(final String cpf);
-    
+
     Owner findByEmail(final String email);
 
 }
