51c21f8da09d3334a809dd9ac65b46a99fffe709
gpm22 <gabriel.milhomem@vorasys.com.br>
2022-03-02 11:10:31 -0300

    apagando imports não utilizados

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Services/impl/VehicleService.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Services/impl/VehicleService.java
@@ -9,9 +9,7 @@ import com.github.gpm22.API_Veiculos.Utils.Commons;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 @Service
