cd10cc7258565a89bb0b8ebe0cee36829d0dc590
gpm22 <gabriel.milhomem@vorasys.com.br>
2022-03-03 13:10:09 -0300

    adicionando findByOwnersIsNull

--- API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/VehicleRepository.java
+++ API_Veiculos/src/main/java/com/github/gpm22/API_Veiculos/Repositories/VehicleRepository.java
@@ -3,6 +3,9 @@ package com.github.gpm22.API_Veiculos.Repositories;
 import com.github.gpm22.API_Veiculos.Entities.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
     Vehicle findByModelAndYear(String model, String year);
+    Set<Vehicle> findByOwnersIsNull();
 }
