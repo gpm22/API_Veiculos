62d807db913da291dec72dfeda55103c6110a8df
Gabriel Milhomem <gabrielpacmil@gmail.com>
2024-04-07 00:45:14 -0300

    Adding test for getFipePrice

--- API_Veiculos/src/test/java/com/github/gpm22/API_Veiculos/Services/VehicleServiceTest.java
+++ API_Veiculos/src/test/java/com/github/gpm22/API_Veiculos/Services/VehicleServiceTest.java
@@ -1,16 +1,39 @@
 package com.github.gpm22.API_Veiculos.Services;
 
+import com.github.gpm22.API_Veiculos.Clients.ApiFipe.ApiFipeClient;
+import com.github.gpm22.API_Veiculos.Clients.ApiFipe.Models.Brand;
+import com.github.gpm22.API_Veiculos.Clients.ApiFipe.Models.Model;
+import com.github.gpm22.API_Veiculos.Clients.ApiFipe.Models.Price;
+import com.github.gpm22.API_Veiculos.Clients.ApiFipe.Models.Year;
 import com.github.gpm22.API_Veiculos.Entities.Vehicle;
+import com.github.gpm22.API_Veiculos.Services.impl.VehicleService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class VehicleServiceTest {
 
-    @Autowired
-    private IVehicleService vehicleService;
+    @InjectMocks
+    private IVehicleService vehicleService = new VehicleService();
+
+    @Mock
+    private ApiFipeClient apiFipeClient;
+
+    private void init() {
+        MockitoAnnotations.openMocks(this);
+        Brand[] brands = new Brand[]{new Brand("A", "1"), new Brand("B", "2")};
+        Mockito.when(apiFipeClient.getBrandList("carros")).thenReturn(brands);
+        Model[] models = new Model[]{new Model("B", "1"), new Model("A", "2")};
+        Mockito.when(apiFipeClient.getModelList("carros", "1")).thenReturn(models);
+        Year[] years = new Year[]{new Year("2005-2", "1"), new Year("2005-1", "2")};
+        Mockito.when(apiFipeClient.getYearList("carros", "1", "1")).thenReturn(years);
+    }
 
     @Test
     public void verifyVehicleInfoTestPass(){
@@ -23,7 +46,6 @@ public class VehicleServiceTest {
         Vehicle fipe = new Vehicle("A", "B", "2432", "fipe");
         vehicleService.verifyVehicleInfo(fipe);
     }
-
 
     @Test
     public void verifyVehicleInfoTestThrow() {
@@ -39,4 +61,25 @@ public class VehicleServiceTest {
         Assertions.assertThrows(IllegalArgumentException.class, () -> vehicleService.verifyVehicleInfo(wrongType));
     }
 
+    @Test
+    public void getFipePriceTestWrongThrow(){
+        init();
+
+        Vehicle nonExistentBrand = new Vehicle("C", "B", "2005-2", "carros");
+        Assertions.assertThrows(IllegalArgumentException.class,  () -> vehicleService.getFipePrice(nonExistentBrand));
+
+        Vehicle nonExistentModel = new Vehicle("A", "C", "2005-2", "carros");
+        Assertions.assertThrows(IllegalArgumentException.class,  () -> vehicleService.getFipePrice(nonExistentModel));
+
+        Vehicle nonExistenYear = new Vehicle("A", "B", "2005-3", "carros");
+        Assertions.assertThrows(IllegalArgumentException.class,  () -> vehicleService.getFipePrice(nonExistenYear));
+    }
+
+    @Test
+    public void getFipePriceTestCorrect(){
+        init();
+        Mockito.when(apiFipeClient.getFipePrice("carros", "1", "1", "1")).thenReturn(new Price("R$ 100.000,00"));
+        Vehicle car = new Vehicle("A", "B", "2005-2", "carros");
+        Assertions.assertEquals( "R$ 100.000,00", vehicleService.getFipePrice(car));
+    }
 }
